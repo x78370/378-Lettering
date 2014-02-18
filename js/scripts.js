aea4efdd9f23afe5e518b76d669126cd7aa6a7e0
Edgar <aedgar1@engr-131-12.eastern.ewu.edu>
2014-02-18 14:45:01 -0800

    1

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,9 @@ $(document).foundation();
 
 // Your Awesome Scripts!
 $(document).ready(function(){
-
+$('h1').lettering();
+$('h3').lettering('words');
+$('h3 + p').lettering('lines')
 	console.log('Page Loaded. Lets Do this!');
 
 }); 
